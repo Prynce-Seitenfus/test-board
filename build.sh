4e75d4d94dda57d157c00cc3ba63eac862406745
PRYNCE IGNACIO SERPA SEITENFUS <prynce.seitenfus@tkelevator.com>
2025-05-06 14:40:10 -0300

    Update pipeline

--- build.sh
+++ build.sh
@@ -3,21 +3,20 @@
 # Disable echo for cleaner output
 set +x
 
-# Check if the script is run as root
-CURRENT_PATH=$(pwd)
-
 # Set the path for STM32CubeIDE
 CUBEIDE_PATH="/opt/st/stm32cubeide_1.18.1"
 export PATH="$CUBEIDE_PATH:$PATH"
 
 # Define the workspace and project
-WORKSPACE="../CURRENT_PATH"
+WORKSPACE="/"
 PROJECT="test-board"
-CONFIG="Debug" # "Debug" or "Release" based on your needs
+CONFIG="Release" # or "Debug"
 
 # Use stm32cubeidec to build the project in the specified workspace
-HEADLESS="--launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core>export XDG_RUNTIME_DIR=/tmp
-xvfb-run "$CUBEIDE_PATH/stm32cubeide" -data "$WORKSPACE" $HEADLESS -import "$WORKSPACE/$PROJEC>xvfb-run "$CUBEIDE_PATH/stm32cubeide" -data "$WORKSPACE" $HEADLESS -build "$PROJECT/$CONFIG"   
+HEADLESS="--launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core
+export XDG_RUNTIME_DIR=/tmp
+xvfb-run "$CUBEIDE_PATH/stm32cubeide" -data "$WORKSPACE" $HEADLESS -import "$WORKSPACE/$PROJEC>xvfb-run "$CUBEIDE_PATH/stm32cubeide" -data "$WORKSPACE" $HEADLESS -build "$PROJECT/$CONFIG"
+xvfb-run "$CUBEIDE_PATH/stm32cubeide" -data "$WORKSPACE" $HEADLESS -build "$PROJECT/$CONFIG"
 
 # Restore the original PATH (optional, if needed)
 unset CUBEIDE_PATH
