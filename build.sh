4022dbe4d85ae7c35ae704d78b05f1d4a368677f
PRYNCE IGNACIO SERPA SEITENFUS <prynce.seitenfus@tkelevator.com>
2025-05-07 09:31:48 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -10,12 +10,12 @@ export PATH="$CUBEIDE_PATH:$PATH"
 # Define the workspace and project
 WORKSPACE=$PWD/..
 PROJECT="test-board"
-CONFIG="Release" # or "Debug"
+CONFIG="Debug" # "Release" or "Debug"
 
 # Use stm32cubeidec to build the project in the specified workspace
 HEADLESS="--launcher.suppressErrors -nosplash -application org.eclipse.cdt.managedbuilder.core
 export XDG_RUNTIME_DIR=/tmp
-xvfb-run "$CUBEIDE_PATH/stm32cubeide" -data "$WORKSPACE" $HEADLESS -import "$WORKSPACE/$PROJEC>xvfb-run "$CUBEIDE_PATH/stm32cubeide" -data "$WORKSPACE" $HEADLESS -build "$PROJECT/$CONFIG"
+xvfb-run "$CUBEIDE_PATH/stm32cubeide" -data "$WORKSPACE" $HEADLESS -import "$WORKSPACE/$PROJECT"
 xvfb-run "$CUBEIDE_PATH/stm32cubeide" -data "$WORKSPACE" $HEADLESS -build "$PROJECT/$CONFIG"
 
 # Restore the original PATH (optional, if needed)
